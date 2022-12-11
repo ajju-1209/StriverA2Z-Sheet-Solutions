bb78cbd1645bb8ceaf3d9f3ec876246c7e5ab5a0
Abhishek Pant <abhishekpant1990@gmail.com>
2022-12-11 20:40:48 +0530

    Sorting

---
+++ SortingTechniques/BubbleSort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+	for (auto x : arr)
+		cout << x << " ";
+	cout << "\n";
+}
